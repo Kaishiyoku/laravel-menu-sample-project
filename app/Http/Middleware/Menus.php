f833b6319d5f542a09bb3e7ef51c5cc77cc84b32
Kaishiyoku <dmp@nfze.de>
2020-09-12 14:52:48 +0200

    added menu samples

---
+++ app/Http/Middleware/Menus.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Menus
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        \LaravelMenu::register()
+            ->addClassNames('mr-auto')
+            ->link('about', 'About')
+            ->link('news.index', 'News')
+            ->dropdown('Content', \LaravelMenu::dropdownContainer()
+                ->link('users.index', 'Users')
+                ->link('articles.index', 'Articles')
+                ->dividerIf(auth()->check())
+                ->headerIf(auth()->check(), 'Favorites')
+                ->linkIf(auth()->check(), 'quotes.index,quotes.create', 'Quotes')
+            )
+            ->text('Sample text')
+            ->content('<div class="mx-2 text-info pt-2">Sample content</div>');
+
+        \LaravelMenu::register('auth')
+            ->linkIf(auth()->guest(), 'login', 'Login')
+            ->linkIf(auth()->guest(), 'register', 'Register')
+            ->linkIf(auth()->check(), 'logout', 'Logout');
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('sample', ['title' => 'Index']);
+});
+
+Route::get('/dummy_auth', function () {
+    if (auth()->check()) {
+        auth()->logout();
+    } else {
+        auth()->loginUsingId(1);
+    }
+
+    return redirect('/');
+})->name('dummy_auth');
+
+$sampleRoutes = collect([
+    'about' => ['about', 'About'],
+    'news.index' => ['news', 'News'],
+    'users.index' => ['users', 'Users'],
+    'articles.index' => ['articles', 'Articles'],
+    'quotes.index' => ['quotes', 'Quotes'],
+    'quotes.create' => ['quotes/create', 'Create quote'],
+    'login' => ['login', 'Login'],
+    'register' => ['register', 'Register'],
+    'logout' => ['logout', 'Logout'],
+]);
+
+$sampleRoutes->each(function ($values, $routeName) {
+    [$uri, $title] = $values;
+
+    Route::get($uri, function () use ($title) {
+        return view('sample', compact('title'));
+    })->name($routeName);
 });
